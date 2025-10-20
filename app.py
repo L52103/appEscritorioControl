e3c62a43c0ac487d4d902962e1c31b5ac888eb5f
sebastián <seba221003@gmail.com>
2025-10-20 13:31:12 -0300

    Se añadió un login para mayor seguridad

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 
 load_dotenv()
 
@@ -14,6 +14,7 @@ from routes.turno import turno_bp
 from routes.turno_trabajador import turno_trabajador_bp
 from routes.reportes import reportes_bp 
 from routes.sueldos import sueldos_bp
+from routes.login import login_bp
 
 app = Flask(__name__)
 
@@ -29,9 +30,17 @@ app.register_blueprint(turno_bp)
 app.register_blueprint(turno_trabajador_bp)
 app.register_blueprint(reportes_bp) 
 app.register_blueprint(sueldos_bp)
+app.register_blueprint(login_bp)
+
 @app.route("/")
+def home():
+    return redirect(url_for('login.login'))
+
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
