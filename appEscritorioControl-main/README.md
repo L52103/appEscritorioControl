bda0997c22d66db3374c4bd847ffa33b1ad241db
Bastiiancoronado <bastiancoronado@gmail.com>
2025-11-19 15:24:42 -0300

    Subiendo proyecto Flask con IA y Predicciones

---
+++ appEscritorioControl-main/README.md
@@ -0,0 +1,9 @@
+En el ultimo codigo, se añadio una nueva función para poder descargar el informe de 
+asistencia en PDF a parte de excel. Se debe instalar la función "WeasyPrint", la cual tiene algunos requisitos.
+
+Descargar el Instalador de GTK+3
+Ve a la página oficial de descargas de GTK para Windows. El enlace directo a la última versión recomendada es este:
+
+https://github.com/tschoonj/GTK-for-Windows-Runtime-Environment-Installer/releases
+
+En la sección "Assets", descarga el archivo .exe. Normalmente se llama algo como gtk3-runtime-x.xx.x-x-x-x-ts-win64.exe.
---
+++ appEscritorioControl-main/app.py
@@ -0,0 +1,47 @@
+
+import os
+from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, url_for
+
+load_dotenv()
+
+from routes.empresa import empresa_bp
+from routes.sucursal import sucursal_bp
+from routes.areatrabajo import area_bp
+from routes.trabajador import trabajador_bp
+from routes.asistencia import asistencia_bp
+from routes.turno import turno_bp
+from routes.turno_trabajador import turno_trabajador_bp
+from routes.reportes import reportes_bp 
+from routes.sueldos import sueldos_bp
+from routes.login import login_bp
+   
+
+app = Flask(__name__)
+
+app.secret_key = os.getenv("SECRET_KEY", "clave_por_defecto_para_dev")
+
+#  blueprints
+app.register_blueprint(empresa_bp)
+app.register_blueprint(sucursal_bp)
+app.register_blueprint(area_bp)
+app.register_blueprint(trabajador_bp)
+app.register_blueprint(asistencia_bp)
+app.register_blueprint(turno_bp)
+app.register_blueprint(turno_trabajador_bp)
+app.register_blueprint(reportes_bp) 
+app.register_blueprint(sueldos_bp)
+app.register_blueprint(login_bp)
+
+@app.route("/")
+def home():
+    return redirect(url_for('login.login'))
+
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
